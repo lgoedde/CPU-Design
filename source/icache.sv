b34823014fe532464b74870b7720f1fd61df6b1b
Michael Baio <mbaio@purdue.edu>
2016-10-15 17:18:00 -0400

    Starting caches by adding files

---
+++ source/icache.sv
@@ -0,0 +1,16 @@
+// interfaces
+`include "datapath_cache_if.vh"
+`include "caches_if.vh"
+
+// cpu types
+`include "cpu_types_pkg.vh"
+
+module icache (
+  input logic CLK, nRST,
+  datapath_cache_if.cache dcif,
+  caches_if cif
+);
+
+  import cpu_types_pkg::*;
+
+endmodule // icache
